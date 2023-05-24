f200ec4eebe0da6163c8cce92bfc254412a5976c
Filip Masny <filip.masny@gmail.com>
2023-05-24 02:14:16 +0200

    update

--- git-pull.sh
+++ git-pull.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-git fetch https://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@github.com/xmasny/beatsaber-map-generator.git
-git pull origin https://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@github.com/xmasny/beatsaber-map-generator.git
+GIT_USER="xmasny"
+GIT_TOKEN="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"
+
+git fetch
+$GIT_TOKEN
+$GIT_USER
+
+git pull
+$GIT_TOKEN
+$GIT_USER
