6cead8edab589a13d3ca61d5ec670f0a203c27a5
Filip Masny <filip.masny@gmail.com>
2023-06-05 01:38:43 +0200

    download ready again

--- download_script.py
+++ download_script.py
@@ -2,14 +2,20 @@ import requests
 import math
 import zipfile
 import os
+import json
+import numpy as np
+from io import TextIOWrapper
 
 
 class DownloadScript:
-    def __init__(self):
+    def __init__(self, file: TextIOWrapper):
+        self.terminal_file = file
         self.songFolder = "data"
         self.zipFiles = []
         self.allUsersJson = []
         self.allMapsUrls = []
+        self.map_info = []
+        self.maps_ids = []
 
     def get_all_users(self, start=0, end=20000):
         # get all users in range of pages
@@ -23,6 +29,8 @@ class DownloadScript:
                     "totalMaps": user['stats']["totalMaps"],
                 })
             print(f"Users page {page} done")
+            self.terminal_file.write(f"Users page {page} done\n")
+            self.terminal_file.flush()
 
     def get_all_maps(self):
         # get all zip maps from users
@@ -31,10 +39,19 @@ class DownloadScript:
                 response = requests.get(
                     f"https://api.beatsaver.com/maps/uploader/{user['id']}/{page}")
                 jsonUserMaps = response.json()
-                print(f"User {user['id']} maps page {page} done")
+                print(
+                    f"User {user['id']} maps page {page} done")
+                self.terminal_file.write(
+                    f"User {user['id']} maps page {page} done\n")
+                self.terminal_file.flush()
                 for map in jsonUserMaps['docs']:
+                    self.map_info.append(map)
+                    self.maps_ids.append(map['id'])
                     for version in map['versions']:
                         self.allMapsUrls.append(version['downloadURL'])
+        with open('map_ids.txt', 'w') as file:
+            for item in self.maps_ids:
+                file.write(str(item) + '\n')
 
     def get_all_maps_from_user(self, start_id=None, end_id=None):
         '''
@@ -48,7 +65,10 @@ class DownloadScript:
             response = requests.get(url)
             with open(f"data/song{index}.zip", 'wb') as f:
                 f.write(response.content)
-            print(f"song{index}.zip downloaded")
+            print(f"song{index}/{len(self.allMapsUrls)-1} downloaded")
+            self.terminal_file.write(
+                f"song{index}/{len(self.allMapsUrls)-1} downloaded\n")
+            self.terminal_file.flush()
 
     def get_all_zips(self):
         # get all zip files
@@ -63,3 +83,14 @@ class DownloadScript:
                 zip_ref.extractall(f"data/{zip_file[:-4]}")
                 # remove song zip file
             os.remove(f"data/{zip_file}")
+
+    def get_all_map_info(self):
+        # get all map info
+        for index, map in enumerate(self.map_info):
+            os.makedirs(f"data/song{index}/generated")
+
+            print(f"song{index}/{len(self.map_info)-1} detail info created")
+            self.terminal_file.write(
+                f"song{index}/{len(self.map_info)-1} detail info created\n")
+            with open(f"data/song{index}/generated/DetailInfo.dat", 'w') as f:
+                json.dump(map, f)
--- main.py
+++ main.py
@@ -1,17 +1,57 @@
 import time
+import datetime
 from download_script import DownloadScript
+import os
+
+folders = ["data", "terminal"]
+
+for folder in folders:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
 
 start_time = time.time()
 
-download = DownloadScript()
 
-download.get_all_users()
-download.get_all_maps()
-download.get_all_maps_from_user()
-download.get_all_zips()
-download.unzip_all_zips()
+def start_date():
+    start_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return start_date
 
-end_time = time.time()
-runtime = end_time - start_time
 
-print("Runtime:", runtime, "seconds")
+print("Start time:", start_date)
+
+
+try:
+    with open(f"terminal/{start_time}.txt", 'a') as file:
+
+        download = DownloadScript(file)
+
+        file.write(f"get_all_users Start time: {start_date()}\n")
+        download.get_all_users()
+
+        file.write(f"get_all_maps Start time: {start_date()}\n")
+        download.get_all_maps()
+
+        file.write(f"get_all_maps_from_user Start time: {start_date()}\n")
+        download.get_all_maps_from_user()
+
+        file.write(f"get_all_zips Start time: {start_date()}\n")
+        download.get_all_zips()
+
+        file.write(f"unzip_all_zips Start time: {start_date()}\n")
+        download.unzip_all_zips()
+
+        file.write(f"get_all_map_info Start time: {start_date()}\n")
+        download.get_all_map_info()
+
+    end_time = time.time()
+    runtime = end_time - start_time
+
+    print("Runtime:", runtime, "seconds")
+
+except Exception as e:
+    with open(f"terminal/{start_time}.txt", 'a') as file:
+        file.write(f"Error time: {start_date()}\n")
+        file.write(f"Error: {e}\n")
+    print("Runtime:", runtime, "seconds")
+    exit()
