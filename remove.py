7d688c27cc3b430f39349bcddc7499d1eb13fc84
Filip Masny <filip.masny@gmail.com>
2023-12-31 17:33:16 +0100

    commit

--- remove.py
+++
@@ -1,8 +0,0 @@
-import json
-import shutil
-with open("saved_data/errored_songs.json", "r") as file:
-    errored_songs = json.load(file)
-
-for song in errored_songs:
-    shutil.rmtree(f"data/{song[0]}")
-    print(f"Removed {song[0]}")
