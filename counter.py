5009319f6b5551f5cd1e9f1ab766c17e848ffceb
Filip Masny <filip.masny@gmail.com>
2024-01-03 23:11:37 +0100

    count unique axes

---
+++ counter.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+pbar = tqdm(os.listdir('dataset/beatmaps/color_notes'))
+axes = []
+
+for file in pbar:
+    if file.endswith(".npy"):
+        # Load mel spectrogram and object data
+        object_array = np.load("dataset/beatmaps/color_notes/" + file)
+
+        for obj in object_array:
+            axes.append(obj[0]) 
+
+print(np.unique(axes))
