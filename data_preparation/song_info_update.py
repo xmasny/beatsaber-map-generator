4b758c1b304f4655448043139d8535225d27cfb8
Filip Masny <filip.masny@gmail.com>
2025-01-23 00:17:24 +0100

    commit

--- data_preparation/song_info_update.py
+++ data_preparation/song_info_update.py
@@ -1,23 +1,23 @@
 import json
+import logging
 import os
 import re
-import pandas as pd
-import requests
-import logging
-
 from csv import QUOTE_NONNUMERIC
 
+import pandas as pd
+import requests
 from tqdm import tqdm
 
 logging.basicConfig(filename="song_info_update.log", level=logging.ERROR)
 
-for type in ("color_notes", "bomb_notes", "obstacles"):
+for type in ("color_notes", ):
 
     path = f"dataset/beatmaps/{type}"
 
     for file in os.listdir(f"dataset/beatmaps/{type}"):
 
         if file.endswith(".csv"):
+            diff_name = file.split(".")[0]
             with open(os.path.join(path, file)) as csvfile:
                 df = pd.read_csv(csvfile, header=None, names=["song", "npz file"])
 
@@ -27,6 +27,7 @@ for type in ("color_notes", "bomb_notes", "obstacles"):
                 df["bpm"] = 0.0
                 df["duration"] = 0
                 df["automapper"] = False
+                df[diff_name] = True
 
             for index, row in tqdm(
                 df.iterrows(), total=len(df), desc=f"Updating {file}"
@@ -58,9 +59,10 @@ for type in ("color_notes", "bomb_notes", "obstacles"):
                     except Exception as e:
                         logging.error(f"Error: {e} - {df.at[index, 'song']}")
                         continue
+            df.drop('npz file', axis=1, inplace=True)
             df.to_csv(
                 os.path.join(path, file),
                 index=False,
-                header=False,
+                header=True,
                 quoting=QUOTE_NONNUMERIC,
             )
