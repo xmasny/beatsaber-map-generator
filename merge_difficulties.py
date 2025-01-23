1a7e1799d71410963e57d6d6777dd3951faa2947
Filip Masny <filip.masny@gmail.com>
2025-01-23 20:48:54 +0100

    update for all objects

--- merge_difficulties.py
+++ merge_difficulties.py
@@ -6,52 +6,39 @@ def aggregate_booleans(series):
 
 
 difficulties = ["Easy", "Normal", "Hard", "Expert", "ExpertPlus"]
+types = ["color_notes", "obstacles", "bomb_notes"]
 
-for difficulty in difficulties:
-    df = pd.read_csv(
-        f"dataset/beatmaps/color_notes/{difficulty}.csv",
-        names=[
-            "song",
-            "upvotes",
-            "downvotes",
-            "score",
-            "bpm",
-            "duration",
-            "automapper",
-        ],
+for object_type in types:
+    merge = pd.concat(
+        [
+            pd.read_csv(f"dataset/beatmaps/{object_type}/{difficulty}.csv")
+            for difficulty in difficulties
+        ]
     )
-    df.drop(df.columns[1], axis=1, inplace=True)
-    df[difficulty] = True
 
-    df.to_csv(f"dataset/beatmaps/color_notes/{difficulty}_old.csv", index=False)
+    merge.to_csv(f"dataset/beatmaps/{object_type}/merged.csv", index=False)
 
+    print(f"Merged {object_type} datasets")
 
-merge = pd.concat(
-    [
-        pd.read_csv(f"dataset/beatmaps/color_notes/{difficulty}.csv")
-        for difficulty in difficulties
-    ]
-)
+    df = pd.read_csv(f"dataset/beatmaps/{object_type}/merged.csv")
 
-merge.to_csv("dataset/beatmaps/color_notes/merged.csv", index=False)
-
-df = pd.read_csv("dataset/beatmaps/color_notes/merged.csv")
-
-combined_df = (
-    df.groupby(
-        ["song", "upvotes", "downvotes", "score", "bpm", "duration", "automapper"]
+    combined_df = (
+        df.groupby(
+            ["song", "upvotes", "downvotes", "score", "bpm", "duration", "automapper"]
+        )
+        .agg(
+            {
+                "Easy": aggregate_booleans,
+                "Normal": aggregate_booleans,
+                "Hard": aggregate_booleans,
+                "Expert": aggregate_booleans,
+                "ExpertPlus": aggregate_booleans,
+            }
+        )
+        .reset_index()
     )
-    .agg(
-        {
-            "Easy": aggregate_booleans,
-            "Normal": aggregate_booleans,
-            "Hard": aggregate_booleans,
-            "Expert": aggregate_booleans,
-            "ExpertPlus": aggregate_booleans,
-        }
-    )
-    .reset_index()
-)
 
-# Save to a new CSV file (optional)
-combined_df.to_csv("dataset/beatmaps/color_notes/combined_songs.csv", index=False)
+    # Save to a new CSV file (optional)
+    combined_df.to_csv(f"dataset/beatmaps/{object_type}/combined_songs.csv", index=False)
+
+    print(f"Combined {object_type} datasets")
