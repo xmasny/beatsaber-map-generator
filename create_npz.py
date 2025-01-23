fa6b8287bd2f2e202b1a1f64136ff2dcdab08d64
Filip Masny <filip.masny@gmail.com>
2025-01-23 21:13:17 +0100

    update npz creation

--- create_npz.py
+++ create_npz.py
@@ -4,31 +4,41 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-os.makedirs("dataset/beatmaps/color_notes/npz", exist_ok=True)
 difficulties = ["Easy", "Normal", "Hard", "Expert", "ExpertPlus"]
+types = ["color_notes", "obstacles", "bomb_notes"]
 
-df = pd.read_csv("dataset/beatmaps/color_notes/combined_songs.csv", header=0)
+for object_type in types:
 
-progress_bar = tqdm(total=len(df))
+    os.makedirs(f"dataset/beatmaps/{object_type}/npz", exist_ok=True)
 
-def load_npy(song):
-    key = song["song"].split(".")[0]
+    df = pd.read_csv(f"dataset/beatmaps/{object_type}/combined_songs.csv", header=0)
 
-    data_dict = {}
+    progress_bar = tqdm(total=len(df), desc=f"Creating {object_type} npz files")
 
-    for difficulty in difficulties:
-        try:
-            if song[difficulty]:
-                data_dict[difficulty] = np.load(f"dataset/beatmaps/color_notes/{difficulty}/{key}.npy")
-        except FileNotFoundError:
-            pass
-        try:
-            data_dict["song"] = np.load(f"dataset/songs/mel229/{key}.npy", allow_pickle=True)
-        except FileNotFoundError:
-            pass
 
-    if data_dict != {}:
-        np.savez(f"dataset/beatmaps/color_notes/npz/{key}.npz", **data_dict)
-    progress_bar.update(1)
+    def load_npy(song):
+        if os.path.exists(f"dataset/beatmaps/{object_type}/npz/{song['song'].split('.')[0]}.npz"):
+            progress_bar.update(1)
+            return
 
-df.apply(load_npy, axis=1)
+        key = song["song"].split(".")[0]
+
+        data_dict = {}
+
+        for difficulty in difficulties:
+            try:
+                if song[difficulty]:
+                    data_dict[difficulty] = np.load(f"dataset/beatmaps/{object_type}/{difficulty}/{key}.npy")
+            except FileNotFoundError:
+                pass
+            try:
+                data_dict["song"] = np.load(f"dataset/songs/mel229/{key}.npy", allow_pickle=True)
+            except FileNotFoundError:
+                pass
+
+        if data_dict != {}:
+            np.savez(f"dataset/beatmaps/{object_type}/npz/{key}.npz", **data_dict)
+        progress_bar.update(1)
+
+
+    df.apply(load_npy, axis=1)
