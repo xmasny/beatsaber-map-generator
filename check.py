596471b143cedc88204d92f63e98c38c3ed73ba3
Filip Masny <filip.masny@gmail.com>
2025-04-09 16:29:51 +0200

    check

--- check.py
+++ check.py
@@ -1,9 +1,19 @@
 import numpy as np
 import os
+import pandas as pd
+
+df = pd.read_csv("dataset/beatmaps/color_notes/combined_songs.csv")
+df["missing_song"] = False
+df["missing_levels"] = False
 
 path = "dataset/beatmaps/color_notes/npz"
-for filename in os.listdir(path):
-    if filename.endswith(".npz"):
-        data = np.load(os.path.join(path, filename), allow_pickle=True)
-        if "song" not in data or len(data) < 2:
-            print(f"Invalid file: {filename}, keys found: {list(data.keys())}")
+for index, song in df.iterrows():
+    data = np.load(os.path.join(path, song["song"]), allow_pickle=True)
+    if "song" not in data:
+        song["missing_song"] = True
+
+    difficulties = ["Easy", "Normal", "Hard", "Expert", "ExpertPlus"]
+    if not any(diff in data for diff in difficulties):
+        song["missing_levels"] = True
+
+df.to_csv("dataset/beatmaps/color_notes/combined_songs.csv", index=False)
