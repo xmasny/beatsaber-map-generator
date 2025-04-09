9561a6a2a9f25d39a13eebcd27667a520a01f016
Filip Masny <filip.masny@gmail.com>
2025-04-09 16:34:24 +0200

    commit

--- check.py
+++ check.py
@@ -2,12 +2,14 @@ import numpy as np
 import os
 import pandas as pd
 
+from tqdm import tqdm
+
 df = pd.read_csv("dataset/beatmaps/color_notes/combined_songs.csv")
 df["missing_song"] = False
 df["missing_levels"] = False
 
 path = "dataset/beatmaps/color_notes/npz"
-for index, song in df.iterrows():
+for index, song in tqdm(df.iterrows(), total=len(df)):
     data = np.load(os.path.join(path, song["song"]), allow_pickle=True)
     if "song" not in data:
         song["missing_song"] = True
