d16c5b3831e72c939e6f06961a40808f1e6df009
Filip Masny <filip.masny@gmail.com>
2025-04-17 22:29:44 +0200

    create_npz

--- create_npz.py
+++ create_npz.py
@@ -11,36 +11,43 @@ logging.basicConfig(
 )
 
 difficulties = ["Easy", "Normal", "Hard", "Expert", "ExpertPlus"]
-types = ["color_notes", "obstacles", "bomb_notes"]
+types = ["color_notes"]
 
 for object_type in types:
 
-    os.makedirs(f"dataset/beatmaps/{object_type}/npz", exist_ok=True)
+    df_metadata = pd.read_csv(f"dataset/beatmaps/{object_type}/metadata.csv", header=0)
+    df_filtered = df_metadata[~df_metadata["automapper"]].copy()
+    df_issues = pd.read_csv(
+        f"dataset/beatmaps/{object_type}/onset_validation_issues.csv", header=0
+    )
 
-    df = pd.read_csv(f"dataset/beatmaps/{object_type}/combined_songs.csv", header=0)
+    df_issues = pd.DataFrame(df_issues["song"].drop_duplicates())
 
-    progress_bar = tqdm(total=len(df), desc=f"Creating {object_type} npz files")
+    for index, issue in tqdm(df_issues.iterrows(), total=len(df_issues)):
 
+        if os.path.exists(f"dataset/beatmaps/{object_type}/npz/{issue['song']}.npz"):
+            os.remove(f"dataset/beatmaps/{object_type}/npz/{issue['song']}.npz")
 
-    def load_npy(song):
-        if os.path.exists(f"dataset/beatmaps/{object_type}/npz/{song['song'].split('.')[0]}.npz"):
-            progress_bar.update(1)
-            return
+        song = df_filtered[df_filtered["song"] == issue["song"]].iloc[0]
 
-        key = song["song"].split(".")[0]
+        key = song["song"]
 
         data_dict = {}
 
         for difficulty in difficulties:
             try:
                 if song[difficulty]:
-                    data_dict[difficulty] = np.load(f"dataset/beatmaps/{object_type}/{difficulty}/{key}.npy")
+                    data_dict[difficulty] = np.load(
+                        f"dataset/beatmaps/{object_type}/{difficulty}/{key}.npy"
+                    )
             except FileNotFoundError:
                 logging.error(f"Map File not found: {key}, {difficulty}")
             except ValueError:
                 try:
-                    data_dict[difficulty] = np.load(f"dataset/beatmaps/{object_type}/{difficulty}/{key}.npy",
-                                                    allow_pickle=True)
+                    data_dict[difficulty] = np.load(
+                        f"dataset/beatmaps/{object_type}/{difficulty}/{key}.npy",
+                        allow_pickle=True,
+                    )
                 except pickle.UnpicklingError as e:
                     logging.error(f"Error loading map {key}, {difficulty}: {e}")
                     print(f"Error loading map {key}, {difficulty}: {e}")
@@ -54,13 +61,14 @@ for object_type in types:
                 print(f"Error loading map {key}, {difficulty}: {e}")
                 continue
         try:
-            data_dict["song"] = np.load(f"dataset/songs/mel229/{key}.npy", allow_pickle=True)
+            data_dict["song"] = np.load(
+                f"dataset/songs/mel229/{key}.npy", allow_pickle=True
+            )
         except FileNotFoundError:
             logging.error(f"Mel File not found: {key}")
         except ValueError as e:
             logging.error(f"Error loading mel file {key}: {e}")
             print(f"Error loading mel file {key}: {e}")
-            return
 
         except pickle.UnpicklingError:
             try:
@@ -68,15 +76,9 @@ for object_type in types:
             except Exception as e:
                 logging.error(f"Error loading mel file {key}: {e}")
                 print(f"Error loading mel file {key}: {e}")
-                return
         except Exception as e:
             logging.error(f"Error loading mel file {key}: {e}")
             print(f"Error loading mel file {key}: {e}")
-            return
 
         if data_dict != {}:
             np.savez(f"dataset/beatmaps/{object_type}/npz/{key}.npz", **data_dict)
-        progress_bar.update(1)
-
-
-    df.apply(load_npy, axis=1)
